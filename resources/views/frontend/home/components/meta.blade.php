c6dbf4b35fa0637b18c7ded86b8d9665ae1d1375
Van Thuong <vanthuonghcmue@gmail.com>
2021-07-09 15:26:05 +0700

    add meta on home page

---
+++ resources/views/frontend/home/components/meta.blade.php
@@ -0,0 +1,13 @@
+@section('meta')
+<meta property="og:url"                content="{{url()->current()}}" />
+<meta property="og:type"               content="website" />
+<meta property="og:title"              content= "Dama. Spend less. Smile more." />
+<meta property="og:description"        content="Free shipping on millions of items. Get the best of Shopping and Entertainment with Dama. Enjoy low prices and great deals on the largest selection of everyday essentials and other products technology, electronics,  video games, and more" />
+<meta property="og:image"              content="{{ asset('frontend/images/banner-sale.jpg') }}" />
+<meta name="description" content="Free shipping on millions of items. Get the best of Shopping and Entertainment with Dama. Enjoy low prices and great deals on the largest selection of everyday essentials and other products technology, electronics,  video games, and more"/>
+<meta name="keywords" content="Dama, Online Shopping, Headphone, Televieion, Electronics, Computers, Cell Phones, Games, watch">
+@endsection
+
+@section('title')
+<title> Dama. Spend less. Smile more.</title>
+@endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,12 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    @include('frontend.components.meta')
     <link rel="shortcut icon" href="{{asset('frontend/images/favicon.png')}}" />
     @include('frontend.components.styles')
-    <title>Home</title>
+    @yield('title')
     <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=60dc7fd8315d7b0012983500&product=sticky-share-buttons" async="async"></script>
 </head>
 <body>
