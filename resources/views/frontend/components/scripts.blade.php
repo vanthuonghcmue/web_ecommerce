b4b8cdc2c5cc13d95a21d274f3ada60c9a306bb9
Van Thuong <vanthuonghcmue@gmail.com>
2021-07-13 10:28:32 +0700

    add gg any

--- resources/views/frontend/components/scripts.blade.php
+++ resources/views/frontend/components/scripts.blade.php
@@ -8,39 +8,65 @@
 <script type="text/javascript" src="{{ asset('frontend/js/store.js') }}"></script>
 <script type="text/javascript" src="{{ asset('frontend/js/bootstrap-toastr/toastr.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('frontend/js/bootstrap-toastr/ui-toastr.js')}}"></script>
-
+<script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "WebSite",
+        "url": "http://damashop.ga/",
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "http://damashop.ga/",
+            "query-input": "required name=search_term"
+        },
+        "name": "My shop",
+        "description": "The best technology all over the world!",
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Ho Chi Minh City, VN",
+            "streetAddress": "351 Lạc Long Quân ,Phường 5, Quận 11, Thành phố Hồ Chí Minh Việt Nam"
+        },
+        "openingHours": [
+            "Mo-Sa 09:00-22:30",
+            "Su 09:00-23:00"
+        ],
+        "telephone": "+84345197641",
+        "image": "http://damashop.ga/frontend/images/banner-sale.jpg",
+        "priceRange": "100"
+    }
+</script>
 <script>
     jQuery(document).ready(function() {
         UIToastr.init();
 
         @if(Session::has('message'))
         var type = "{{ Session::get('alert-type', 'success') }}";
-        switch(type){
-        case 'info':
-            toastr.info("{{ Session::get('message') }}");
-            break;
-        case 'warning':
-            toastr.warning("{{ Session::get('message') }}");
-            break;
-        case 'success':
-            toastr.success("{{ Session::get('message') }}");
-            break;
-        case 'error':
-            toastr.error("{{ Session::get('message') }}");
-            break;
-        default:
-            toastr.success("{{ Session::get('message') }}");
-            break;
+        switch (type) {
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+                break;
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+                break;
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+                break;
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+                break;
+            default:
+                toastr.success("{{ Session::get('message') }}");
+                break;
         }
         @endif
     });
 
-    $(document).on("click", ".add_to_cart", function () {
+    $(document).on("click", ".add_to_cart", function() {
         let product_id = $(this).attr("data-id");
         let product_sku_id = $(this).attr("data-sku_id");
         let quantity = 1;
 
-        url = '{{ route('cart.add_to_cart') }}';
+        url = '{{ route('
+        cart.add_to_cart ') }}';
         body = {
             id: product_id,
             product_sku_id: product_sku_id,
@@ -62,7 +88,7 @@
         const response = await fetch(url, {
             method: 'POST',
             headers: {
-            'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         });
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,6 +7,25 @@
     @include('frontend.components.styles')
     @yield('title')
     <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=60dc7fd8315d7b0012983500&product=sticky-share-buttons" async="async"></script>
+    <meta name="google-site-verification" content="q-OSpHnocyz6yrS-Wj9RarASDK8OX_ty628zXMRSdLE" />
+    <!-- Google Analytics -->
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'G-XTTKYEWPJY', 'auto');
+        ga('send', 'pageview');
+    </script>
+    <!-- End Google Analytics -->
 </head>
 <body>
     @include('frontend.components.header')
