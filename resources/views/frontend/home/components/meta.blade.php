a28835abb29506b845ce694c0c1c57c620a3442e
Van Thuong <vanthuonghcmue@gmail.com>
2021-07-18 22:08:32 +0700

    add open grap product detail

--- resources/views/frontend/home/components/meta.blade.php
+++ resources/views/frontend/home/components/meta.blade.php
@@ -1,3 +1,6 @@
+@section('title')
+<title>Dama | Spend less | Smile more</title>
+@endsection
 @section('meta')
 <meta name="description" content="Free shipping on millions of items. Get the best of Shopping and Entertainment with Dama. Enjoy low prices and great deals on the largest selection of everyday essentials and other products technology, electronics,  video games, and more"/>
 <meta name="keywords" content="Dama, Online Shopping, Headphone, Televieion, Electronics, Computers, Cell Phones, Games, watch">
@@ -15,6 +18,4 @@
 <meta name="twitter:image" content=" asset('frontend/images/banner-sale.jpg')">
 @endsection
 
-@section('title')
-<title>Dama | Spend less | Smile more</title>
-@endsection
+
